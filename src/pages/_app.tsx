a9df126e9809ef7d6dcad64cfcda8090e1dfa25a
killer-cf <costa.kilder@live.com>
2023-03-10 17:09:57 -0300

    feat: carousel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app'
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+
+import logo from '../assets/logo.png'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logo} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>)
+  
 }
