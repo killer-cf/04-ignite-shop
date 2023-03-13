a1e891df2556344e9c822ee04bad3e072eaffb16
killer-cf <costa.kilder@live.com>
2023-03-13 15:41:48 -0300

    fix: product link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { HomeContainer, Product } from '@/styles/pages/home'
 import Image from 'next/image'
 import { useKeenSlider } from 'keen-slider/react'
+import Link from 'next/link'
 
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from '@/lib/stripe'
@@ -29,14 +30,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map(product => {
         return (
-          <Product key={product.id} className='keen-slider__slide'>
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link href={`/product/${product.id}`}  key={product.id}>
+            <Product className='keen-slider__slide'>
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
 
